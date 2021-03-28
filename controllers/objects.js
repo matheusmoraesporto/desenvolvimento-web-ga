d6ddf4af979c1dd3f4bd871c53a9df30ee2f4dc3
matheusmoraesporto99@gmail.com <matheusmoraesporto99@gmail.com>
2021-03-28 01:18:53 -0300

    Criação de classe para retornar objetos, simulando resposta de um API

---
+++ controllers/objects.js
@@ -0,0 +1,282 @@
+class Objects {
+    static getProductsToSell() {
+        return [
+            {
+                id: 1,
+                descricao: "Atari 2600",
+                img: "../assets/objectImages/Atari 2600.png",
+                valor: 1299.99
+            },
+            {
+                id: 1,
+                descricao: "Xbox series S",
+                img: "../assets/objectImages/Atari 2600.png",
+                valor: 1299.99
+            },
+            {
+                id: 1,
+                descricao: "Xbox series S",
+                img: "",
+                valor: 1299.99
+            },
+            {
+                id: 1,
+                descricao: "Xbox series S",
+                img: "",
+                valor: 1299.99
+            },
+            {
+                id: 1,
+                descricao: "Xbox series S",
+                img: "",
+                valor: 1299.99
+            },
+            {
+                id: 1,
+                descricao: "Xbox series S",
+                img: "",
+                valor: 1299.99
+            },
+            {
+                id: 1,
+                descricao: "Xbox series S",
+                img: "",
+                valor: 1299.99
+            },
+            {
+                id: 1,
+                descricao: "Xbox series S",
+                img: "",
+                valor: 1299.99
+            },
+            {
+                id: 1,
+                descricao: "Xbox series S",
+                img: "",
+                valor: 1299.99
+            },
+            {
+                id: 1,
+                descricao: "Xbox series S",
+                img: "",
+                valor: 1299.99
+            },
+            {
+                id: 1,
+                descricao: "Xbox series S",
+                img: "",
+                valor: 1299.99
+            },
+            {
+                id: 1,
+                descricao: "Xbox series S",
+                img: "",
+                valor: 1299.99
+            },
+            {
+                id: 1,
+                descricao: "Xbox series S",
+                img: "",
+                valor: 1299.99
+            },
+            {
+                id: 1,
+                descricao: "Xbox series S",
+                img: "",
+                valor: 1299.99
+            },
+            {
+                id: 1,
+                descricao: "Xbox series S",
+                img: "",
+                valor: 1299.99
+            },
+            {
+                id: 1,
+                descricao: "Xbox series S",
+                img: "",
+                valor: 1299.99
+            },
+            {
+                id: 1,
+                descricao: "Xbox series S",
+                img: "",
+                valor: 1299.99
+            },
+            {
+                id: 1,
+                descricao: "Xbox series S",
+                img: "",
+                valor: 1299.99
+            },
+            {
+                id: 1,
+                descricao: "Xbox series S",
+                img: "",
+                valor: 1299.99
+            },
+            {
+                id: 1,
+                descricao: "Xbox series S",
+                img: "",
+                valor: 1299.99
+            },
+            {
+                id: 1,
+                descricao: "Xbox series S",
+                img: "",
+                valor: 1299.99
+            },
+            {
+                id: 1,
+                descricao: "Xbox series S",
+                img: "",
+                valor: 1299.99
+            },
+            {
+                id: 1,
+                descricao: "Xbox series S",
+                img: "",
+                valor: 1299.99
+            },
+            {
+                id: 1,
+                descricao: "Xbox series S",
+                img: "",
+                valor: 1299.99
+            },
+            {
+                id: 1,
+                descricao: "Xbox series S",
+                img: "",
+                valor: 1299.99
+            },
+            {
+                id: 1,
+                descricao: "Xbox series S",
+                img: "",
+                valor: 1299.99
+            },
+            {
+                id: 1,
+                descricao: "Xbox series S",
+                img: "",
+                valor: 1299.99
+            },
+            {
+                id: 1,
+                descricao: "Xbox series S",
+                img: "",
+                valor: 1299.99
+            },
+            {
+                id: 1,
+                descricao: "Xbox series S",
+                img: "",
+                valor: 1299.99
+            },
+            {
+                id: 1,
+                descricao: "Xbox series S",
+                img: "",
+                valor: 1299.99
+            },
+            {
+                id: 1,
+                descricao: "Xbox series S",
+                img: "",
+                valor: 1299.99
+            },
+            {
+                id: 1,
+                descricao: "Xbox series S",
+                img: "",
+                valor: 1299.99
+            },
+            {
+                id: 1,
+                descricao: "Xbox series S",
+                img: "",
+                valor: 1299.99
+            },
+            {
+                id: 1,
+                descricao: "Xbox series S",
+                img: "",
+                valor: 1299.99
+            },
+            {
+                id: 1,
+                descricao: "Xbox series S",
+                img: "",
+                valor: 1299.99
+            },
+            {
+                id: 1,
+                descricao: "Xbox series S",
+                img: "",
+                valor: 1299.99
+            },
+            {
+                id: 1,
+                descricao: "Xbox series S",
+                img: "",
+                valor: 1299.99
+            },
+            {
+                id: 1,
+                descricao: "Xbox series S",
+                img: "",
+                valor: 1299.99
+            },
+            {
+                id: 1,
+                descricao: "Xbox series S",
+                img: "",
+                valor: 1299.99
+            },
+            {
+                id: 1,
+                descricao: "Xbox series S",
+                img: "",
+                valor: 1299.99
+            },
+            {
+                id: 1,
+                descricao: "Xbox series S",
+                img: "",
+                valor: 1299.99
+            },
+            {
+                id: 1,
+                descricao: "Xbox series S",
+                img: "",
+                valor: 1299.99
+            },
+            {
+                id: 1,
+                descricao: "Xbox series S",
+                img: "",
+                valor: 1299.99
+            },
+            {
+                id: 1,
+                descricao: "Xbox series S",
+                img: "",
+                valor: 1299.99
+            },
+            {
+                id: 1,
+                descricao: "Xbox series S",
+                img: "",
+                valor: 1299.99
+            },
+            {
+                id: 1,
+                descricao: "Xbox series S",
+                img: "",
+                valor: 1299.99
+            }
+        ];
+    }
+}
