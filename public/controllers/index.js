c3cb41b541242af258908761792e06cbf2593dcd
matheusmoraesporto99@gmail.com <matheusmoraesporto99@gmail.com>
2021-04-12 21:58:19 -0300

    Commit com rascunho da listagem

--- public/controllers/index.js
+++ public/controllers/index.js
@@ -1,7 +1,32 @@
-let main = document.getElementsByClassName('main')[0];
+let products = document.getElementsByClassName('products')[0];
 let content = '';
-let itens = ['controle', 'console'];
-itens.forEach(i => {
-    content += `<p>${i}</p>`
+
+
+function readTextFile(file, callback) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.overrideMimeType("application/json");
+    rawFile.open("GET", file, true);
+    rawFile.onreadystatechange = function() {
+        if (rawFile.readyState === 4 && rawFile.status == "200") {
+            callback(rawFile.responseText);
+        }
+    }
+    rawFile.send(null);
+}
+
+//usage:
+readTextFile("./data/produtos.json", function(text){
+    let items = JSON.parse(text);//.slice(0, 3);
+    
+    items.forEach(i => {
+        content += `
+        <div>
+            <img src="${i.img}" alt="Carrinho" class="cursor-pointer">
+            <p>${i.description}</p>
+        </div>
+        `;
+    });
+
+    Utils.pushComponent(products, content);
 });
-Utils.pushComponent(main, content);
+
