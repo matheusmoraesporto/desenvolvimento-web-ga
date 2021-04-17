66d29b9ee041345ba3346c4c344ac49f4217ab0d
matheusmoraesporto99@gmail.com <matheusmoraesporto99@gmail.com>
2021-04-17 19:43:02 -0300

    Implementação dos filtros dos produtos

--- public/controllers/index.js
+++ public/controllers/index.js
@@ -1,7 +1,11 @@
 let productsComponent = document.getElementsByClassName('products')[0],
+    inputSearch = document.getElementById('search'),
+    btnFind = document.getElementsByClassName('find')[0],
+    filtersTypeProduct = Array.prototype.slice.call(document.getElementsByClassName('filters-type-product')),
     products = [],
     cart = [],
-    content = '',
+    filtersType = [],
+    filtersBranch = [],
     loadProducts = new Request('http://localhost:3000/products');
 
 fetch(loadProducts)
@@ -11,14 +15,17 @@ fetch(loadProducts)
         }
     })
     .then(response => {
+        products = response;
         renderProducts(response);
     })
     .catch(error => {
         alert(error);
     });
 
-renderProducts = (products) => {
-    products.forEach((product) => {
+renderProducts = (items) => {
+    let content = '';
+
+    items.forEach((product) => {
         content += `
         <div class="card-products">
             <img src="${product.img}" alt="Carrinho" class="products-images">
@@ -48,7 +55,7 @@ renderProducts = (products) => {
 
             let id = event.srcElement.id.replace('btn-add-product-', ''),
                 spnCart = document.getElementById('cart-length'),
-                newProduct = products.filter(o => o.id == id)[0];
+                newProduct = items.filter(o => o.id == id)[0];
 
             // Aqui adiciona o item no carrinho se ele ainda não estiver lá, se não tem que incrementar a quantidade
             if (cart.some(o => o.id === newProduct.id)) {
@@ -75,3 +82,55 @@ renderProducts = (products) => {
         });
     });
 };
+
+filtersTypeProduct.forEach(o => {
+    o.addEventListener('click', onClick);
+});
+
+function onClick(event) {
+    let { id, checked } = event.srcElement,
+        productsFiltered = [],
+        property = ['Console', 'Acessorio'].includes(id) ? 'type' : 'branch';
+
+    if (property === 'type') {
+        filtersType = defineFilter(filtersType, checked, id);
+    }
+    else {
+        filtersBranch = defineFilter(filtersBranch, checked, id);
+    }
+
+    productsFiltered = products.filter(o =>
+        (filtersType.length === 0 || filtersType.includes(o.type))
+        && (filtersBranch.length === 0 || filtersBranch.includes(o.branch)));
+
+    renderProducts(productsFiltered);
+};
+
+defineFilter = (items, checked, id) => {
+    if (checked) {
+        items.push(id);
+    }
+    else {
+        items = items.filter(o => o !== id);
+    }
+
+    return items;
+}
+
+btnFind.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    if (inputSearch.value.length > 0) {
+        let productsSerach = products.filter(o => o.description.toLowerCase().includes(inputSearch.value.toLowerCase()));
+        renderProducts(productsSerach);
+    }
+    else {
+        renderProducts(products);
+    }
+});
+
+inputSearch.addEventListener('change', () => {
+    if (!inputSearch.value) {
+        renderProducts(products);
+    }
+});
