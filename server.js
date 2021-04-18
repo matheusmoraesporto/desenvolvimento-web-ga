652b046d3428fec873622d00b6cdf98367ba20d0
matheusmoraesporto99@gmail.com <matheusmoraesporto99@gmail.com>
2021-04-18 12:49:07 -0300

    Exibir resumo dos produtos que estão no carrinho

--- server.js
+++ server.js
@@ -8,8 +8,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/products', (req, res) => {
+    let response = products.sort((a, b) => b.value - a.value);
+
     res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify(products));
+    res.end(JSON.stringify(response));
 });
 
 app.listen(3000, () => { });
