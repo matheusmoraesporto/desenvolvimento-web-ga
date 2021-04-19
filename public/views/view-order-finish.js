c6b159502343124dc0bba29b3817da81a0e2b63c
Jennifer Diehl <jenniferdiehll@hotmail.com>
2021-04-18 21:41:50 -0300

    Tela finalizacao pedido

--- public/views/view-order-finish.js
+++ public/views/view-order-finish.js
@@ -1,22 +1,22 @@
 class ViewOrderFinish {
     static getOrderFinish() {
         return `
-            <div>
-                <div class="flex-row">
-                    <div>
+            <div class="card-conclusion">
+                <div class="product-resume">
+                    <div class="resume">
                         <h3>Resumo do pedido</h3>
                         <p>Número do pedido: <span>${parseInt(Math.random() * 100000)}</span></p>
-                        <p>Valor do pedido: <span>R$ESTÁ FIXO NEGÃO</span></p>
+                        <p>Valor do pedido:<span>R$ ESTÁ FIXO NEGÃO</span></p>
                     </div>
-                    <div>
+                    <div class="conclusion">
                         <h3>Pedido conlcluído com sucesso!</h3>
                         <img src="../assets/Icons/ok.png" alt="Sucesso" class="">
                     </div>
                 </div>
                 
-                <div class="flex-row">
+                <div class="payment">
                     <p>O boleto de cobrança foi enviado para o seu e-mail.</p>
-                    <button type="submit" id="back" class="cursor-pointer">Voltar ao início <img src="../assets/Icons/log-out.svg" alt="Voltar" class=""></button>
+                    <button type="submit" id="back" class="cursor-pointer">Voltar ao início <img src="../assets/Icons/log-out.png" alt="Voltar" class=""></button>
                 </div>
             </div>`;
     }
